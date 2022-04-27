61421f2035aef470c847c7762f085515a7aaf33e
MHargrove96 <MichaelH.96@yahoo.com>
2022-04-27 17:50:15 -0500

    saving work on the header bar

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -1,23 +1,23 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
-import LogoLink from './LogoLink'
-import SearchBarInput from './SearchBarInput'
+import LogoLink from "./LogoLink";
+import SearchBarInput from "./SearchBarInput";
+import SettingsButton from "./SettingsButton";
 
-import '../styles/HeaderBox.css'
+import "../styles/HeaderBox.css";
 
 function HeaderBar(props) {
-
-
-
   return (
-    <header className='HeaderBox'>
-      <LogoLink/>
-      <h3 id='SearchLabel'>Search
-      <br/>
-       Hacker News</h3>
-       <SearchBarInput/>
+    <header className="HeaderBox">
+      <LogoLink />
+      <span className="SeachLabelWrapper">
+        <h3 className="SearchLabel">Search </h3>
+        <h3 className="SearchLabel">Hacker News</h3>
+      </span>
+      <SearchBarInput />
+      <SettingsButton />
     </header>
-  )
+  );
 }
 
-export default HeaderBar
+export default HeaderBar;
