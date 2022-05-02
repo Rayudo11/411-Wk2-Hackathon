8efb74dde4fb7f546134eccf6685ea70d4557d1d
MHargrove96 <MichaelH.96@yahoo.com>
2022-05-02 17:54:20 -0500

    saving work no important changes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
 import React, { Component } from "react";
-import axios from 'axios';
+import axios from "axios";
 
-import './styles/App.css'
+import "./styles/App.css";
 
-import './components/HeaderBar'
+import "./components/HeaderBar";
 import HeaderBar from "./components/HeaderBar";
 
 class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
 
-    this.state ={
+    this.state = {
       arrayOfStories: [],
-    }
+      searchTerm: "",
+    };
   }
 
-  componentDidMount(){
-    axios.get("https://hn.algolia.com/api").then((res) => {
-      const arrayOfStories = res.data;
-      this.setState({arrayOfStories});
+  componentDidMount() {
+    axios.get('https://hn.algolia.com/api/v1/search?query=...')
+    .then((res) => {
+      const arrayOfStories = res.data.hits
+      this.setState({ arrayOfStories })
+    })
+  }
 
-    });
-  };
-
-  render(){
+  render() {
     return (
       <div className="App">
-      <HeaderBar/>
-    </div>
-  );
+        <HeaderBar />
+        {/* map over this,state.arrayOfStories */}
+        <ol>{this.state.arrayOfStories.map((hit, i) => {
+          return (
+            <li key={i}>
+              <h5>{hit.title}</h5>
+              </li>
+          )
+        })}</ol>
+      </div>
+    );
   }
-  
 }
 
 export default App;
