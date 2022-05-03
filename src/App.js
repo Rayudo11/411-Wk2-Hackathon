559f9719b3952b51164c79b0ab2e06cf9533f31f
David Castillo <seth.castillo4@gmail.com>
2022-05-03 16:55:30 -0500

    Finished my portion of project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
 import List from "./components/List";
-
 import './styles/App.css'
 
 import './components/HeaderBar'
@@ -9,33 +7,19 @@ import HeaderBar from "./components/HeaderBar";
 
 let App = () => {
 
- let [apiData,setApiData] = useState([])
-
- // let getData = async () => {
- //  let url = 'https://hn.algolia.com/api/v1/search_by_date?tags=story'
- //  let qry = (await (axios.get(url))).data.hits
- //  setApiData(qry)
- // }
+ let [apiData, setApiData] = useState([])
 
  useEffect(() => {
-  // getData()
-  fetch('https://hn.algolia.com/api/v1/search_by_date?tags=story').then(res => res.json()).then(data => setApiData(data.hits))
+  fetch('https://hn.algolia.com/api/v1/search_by_date?tags=story')
+  .then(res => res.json())
+  .then(data => setApiData(data.hits))
  },[])
 
- useEffect(() => {
-  console.log(apiData)
- },[apiData])
-
-    return (
-      <div className="App">
-      <HeaderBar/>
-      <ul>
-      {apiData.length !== 0 && apiData.map((story,ix) => {
-        <li key={ix}>{story.title}</li>
-      })}
-     </ul>
-      {/* <List data={apiData}/> */}
-    </div>
+ return (
+  <div className="App">
+   <HeaderBar />
+   <List data={apiData}/>
+    </div >
   );
 }
 
--- src/components/List.js
+++ src/components/List.js
@@ -1,23 +1,45 @@
 import React from 'react'
+import '../styles/List.css'
 
 let List = (props) => {
 
  let apiData = Object.values(props.data) 
-
  console.log(apiData)
 
-  // if (props.data.length === 0) {
-  //   return null
-  //  }
-  //  else {
+ let getTime = (date) => {
+  let today = new Date()
+  let currentTime = today.getMinutes()
+  let pubDate = date.substr(14,2)
+  let timeDifference = 0
+
+  if (pubDate > currentTime) {
+   timeDifference = pubDate - currentTime
+  } else {timeDifference = currentTime - pubDate}
+  
+  return (
+   <>
+   {timeDifference} minutes ago
+   </>
+  )
+ }
+
     return (
-     <ul>
-      {props.data.length === 0 ? null : apiData.map((story,ix) => {
-        <li>{story.title}</li>
-      })}
-     </ul>
+    <ul id="dataUL">
+     {apiData.map((item, ix) =>
+      <li key={ix} className="mapData">
+       <section> 
+        <span className='title'>{item.title}</span> <a className='url' href={item.url}>({item.url})</a>
+       </section>
+       <section className='details'>
+        <a className='points'> {item.points} points</a> | 
+        <a className='author'> {item.author}</a> | 
+        <a className='time'> {getTime(item.created_at)}</a> | 
+        <a className='comments'> {item.num_comments} comments</a>
+       </section>
+      </li>
+     )}
+  </ul>
     )
-   // }
 }
 
 export default List
